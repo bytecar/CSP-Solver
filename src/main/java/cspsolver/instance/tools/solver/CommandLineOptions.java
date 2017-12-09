8eda53eb9685d1bac4a021ca050299bca033fd0c
Unknown <bytecar@gmail.com>
2017-12-09 09:00:35 +0530

    Remove .idea folder from git.

--- src/main/java/cspsolver/instance/tools/solver/CommandLineOptions.java
+++ src/main/java/cspsolver/instance/tools/solver/CommandLineOptions.java
@@ -12,8 +12,8 @@ public class CommandLineOptions {
 		options.addOption("u", "heuristics", true, "Variable Ordering Heuristic: \nLX(lexicographic), \nLD(least domain), \nDEG(max degree), \nDD(domain degree ratio), \nW(Minimum Width), \nLX is used if not specified\n");
 		options.addOption("ud","heuristics-dynamicity", true, "Variable Ordering Heuristic Dynamicity: \n static \n dynamic, \n statically ordered if not specified");
 		options.addOption("s", "solution", true, "Solutions: \n\"1\" for single solution, \n\"all\" for all solutions,\n prints all solution if not specified\n");
-		options.addOption("f", "inputfile", true, "Path to file in XCSP 2.0 format\n");
-		options.addOption("p", "printsolutions", true, "Optional: Append \n\"-p p\" to print solutions, \n\"-p n\" not to print solutions,\nprints solution if not specified\n");
+		options.addOption("f", "input-file", true, "Path to file in XCSP 2.0 format\n");
+		options.addOption("p", "print-solutions", true, "Optional: Append \n\"-p p\" to print solutions, \n\"-p n\" not to print solutions,\nprints solution if not specified\n");
 		options.addOption("h", "help", false, "Help for using Solver\n");
 		options.addOption("d", "debug", false, "CSP debug functionality,\ndisabled if not specified\n");
 	}
